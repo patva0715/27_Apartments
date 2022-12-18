b8d0b8ccee17422b00697ecd25bef8480c6a68b0
PV <patrickvalera@patMB.lan>
2022-12-17 20:49:20 -0800

    font links

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -21,9 +21,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
-        {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
-        {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet" /> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet" />
       </Head>
       {loadMap ?
         <MapBG showMap={showMap} visibleLocations={visibleLocations} />
